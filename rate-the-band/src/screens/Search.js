d38ca099829d5eb8525de494d2d7ea1da57ce282
Pedro Soares <pedrowesleyfs@gmail.com>
2023-05-25 02:00:56 -0300

    abstracts the hook useBandSearch

--- rate-the-band/src/screens/Search.js
+++ rate-the-band/src/screens/Search.js
@@ -6,10 +6,11 @@ import { BandCard } from '../common/BandCard/BandCard';
 import { BandCardLoader } from '../common/BandCard/BandCardLoader';
 import styled from 'styled-components';
 import $ from 'jquery';
-import {HeadingThree} from '../common-components/Tipografia/HeadingThree';
-import {InfoApresentation} from '../common/InfoApresentation/InfoApresentation';
-import { Button } from "../common-components/Button/Button"	;
+import { HeadingThree } from '../common-components/Tipografia/HeadingThree';
+import { InfoApresentation } from '../common/InfoApresentation/InfoApresentation';
+import { Button } from "../common-components/Button/Button";
 import searchIcon from '../assets/icons/search.svg';
+import { useBandSearch } from '../hooks/useBandSearch';
 
 const BandGrid = styled(Box)`
 
@@ -38,12 +39,8 @@ const BandGrid = styled(Box)`
 		
 	}
 
-		gap: ${Spaces.ONE_HALF};
-	@media (max-width: 400px) {
-		grid-template-columns: 1fr;
-		gap: ${Spaces.TWO};
 		
-	}
+
 `;
 
 const Icon = styled.img.attrs({
@@ -76,39 +73,6 @@ const Wrapper = styled.div`
 
 export function Search() {
 
-function searchBand(band) {
-  const url = `http://api.deezer.com/search?q=artist:"${band}"&output=jsonp`;
-	setIsLoading(true);
-  return new Promise((resolve, reject) => {
-    $.ajax({
-      type: 'GET',
-      url,
-      dataType: 'jsonp',
-      contentType: 'application/json; charset=utf-8',
-      jsonpCallback: 'jsonCallback',
-      cache: false,
-      beforeSend: (xhr) => {
-        // função antes de executar a chamada
-      },
-      success: (data) => {
-        resolve(data);
-      },
-      error: (xhr, status, error) => {
-        reject(error);
-      }
-    });
-  });
-}
-
-
-
-	const [bands, setBands] = React.useState([]);
-	const [search, setSearch] = React.useState({
-		value: null,
-		doSearch: false,
-	});
-	const [isLoading, setIsLoading] = React.useState(true);
-
 	function handleUpdateSearchValue({target: {value} }){
 		setSearch((prevValue) => ({... prevValue, value}))
 	}
@@ -117,69 +81,22 @@ function searchBand(band) {
 		setSearch((prevValue) => ({ ...prevValue, doSearch: true }));
 	}
 
+	const [bandName, setBandName] = React.useState ('rock')
+	const [search, setSearch] = React.useState({
+		value: "",
+		doSearch: false,
+	});
+	
 	React.useEffect(() => {
 		if (search.doSearch) {
-			searchBand(search.value).then((bands) => {
-			setBands(newBand(bands.data));
+		setBandName(search.value)
 			setSearch((prevValue) => ({ ...prevValue, doSearch: false}));
-			
-			setIsLoading(false)
-		});
+				
 	}
 	},[search]);
 
-	React.useEffect(() => {
-		searchBand('rock').then((bands) => {
 
-			setBands(newBand(bands.data))
-			// console.log(newBand(bands.data))
-			setIsLoading(false);
-		});
-	}, []);
-
-
-	
-
-	//a API do deezer retorna varios albuns com diversos artistas repetidos, essa função retorna um map organizado com as informações de cada artista/banda e todos os albuns relacionados a ele
-	function newBand(bandas) {
-
-		let bandaUnica = new Map();
-		const bandArray = bandas.filter((valorAtual) =>{
-			
-			//esse trecho cria um array apenas com os albuns correspondentes ao id do artista do valorAtual
-
-			let bandAlbums = []
-			const albunsFilter = bandas.filter ((albumAtual, indice) =>{
-				
-
-
-				if (albumAtual.artist.id === valorAtual.artist.id ) {
-					bandAlbums.push(albumAtual.album)
-			}});
-
-			let bandMusics = []
-			const musicsFilter = bandas.filter ((musicaAtual) =>{
-				if (musicaAtual.artist.id === valorAtual.artist.id) {
-					bandMusics.push(musicaAtual.title)
-			}});
-
-			// esse trecho confere se o valor já foi inserido antes na 'bandaUnica' e se não tiver, ele retorna o map da banda com os albuns de cada artista
-			if (!bandaUnica.has(valorAtual.artist.id)) {
-				      const bandObject = {
-						artist: valorAtual.artist,
-						albuns: bandAlbums,
-						musics: bandMusics
-      }
-				bandaUnica.set(valorAtual.artist.id, bandObject)
-			}
-		})
-
-		
-		return ([...bandaUnica.values()])
-
-	}
-
-
+	const {bands, isLoading} = useBandSearch(bandName)
 
 	return (
 		
